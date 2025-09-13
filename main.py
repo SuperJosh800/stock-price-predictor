d32e7e73c8f4f98f7c6be42352094a2958cb290a
Joshua Conetta <147725463+SuperJosh800@users.noreply.github.com>
2025-09-13 16:39:08 +0100

    Added data download from yfiance

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Test print")
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+tickers = ['SPY', 'BIRD','GLD','VTI', 'QQQ'] # SPDR S&P 500 ETF (SPY), Allbirds, Inc. (BIRD), SPDR Gold Shares (GLD), Vanguard Total Stock Market Index Fund ETF Shares (VTI), Invesco QQQ Trust (QQQ)
+
+end_date = datetime.today()
+print(end_date)
+
+start_date = end_date - timedelta(days = 2*365)
+print(start_date)
+
+close_df = pd.DataFrame()
+
+for ticker in tickers:
+    data = yf.download(ticker, start=start_date, end=end_date)
+    close_df[ticker] = data['Close']
+
+    print(close_df)
